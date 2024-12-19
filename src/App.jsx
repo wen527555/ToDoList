b04879e80e8384b13df2f129912a495fc463fb1b
wen527555 <wen527555@gmail.com>
2024-12-19 14:08:00 +0800

    feat:develop add task

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,42 @@ import GlobalStyle from "./styles/globalStyles";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
-import TodoList from "./components/TodoItem";
-import InputBox from "./components/InputBox";
+import TodoItem from "./components/TaskItem";
+import TaskInput from "./components/TaskInput";
+import { useState } from "react";
+
+const generateId = (() => {
+  let id = 0;
+  return () => id++;
+})();
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: generateId(), text: "一天一蘋果，醫生遠離我", completed: false },
+    { id: generateId(), text: "投籃1000次", completed: false },
+    { id: generateId(), text: "完成 pre test作業", completed: true },
+  ]);
+
+  const handleAddTask = (text) => {
+    setTasks([...tasks, { id: generateId(), text, completed: false }]);
+  };
+
+  const handleToggleTodo = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !TodoItem.completed } : task
+      )
+    );
+  };
+
   return (
     <>
       <GlobalStyle />
       <Header />
       <Content>
         <Progress />
-        <TodoList />
-        <InputBox />
+        <TodoItem tasks={tasks} onToggle={handleToggleTodo} />
+        <TaskInput onAdd={handleAddTask} />
       </Content>
     </>
   );
